c40b7fb2a88f4e230f93b70ffc746f4f5bb8bac1
Sergejus <Sergejus@DESKTOP-VE3RPFL>
2023-02-07 20:39:50 +0200

    Add project files.

---
+++ Calculator2/Calculator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator2
+{
+    public class Calculator
+    {
+        public static int Sum(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        public static double SumDouble3(double number1, double number2, double number3)
+        {
+            return (double)number1 + number2 + number3;
+        }
+
+        public static int Difference(int number1, int number2)
+        {
+            return number1 - number2;
+        }
+
+
+        public static int Product(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+
+        public static double Quotient(int number1, int number2)
+        {
+            return number1 / number2;
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
---
+++ CalculatorUnitTests/UnitTests.cs
@@ -0,0 +1,43 @@
+﻿using Calculator2;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorUnitTests
+{
+    public class UnitTests
+    {
+        public void Sum1And1()
+        {
+            Calculator2.Calculator.Sum(1, 1);
+
+            int expectedResult = 2;
+
+            int actualResult = Calculator2.Calculator.Sum(1, 1);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [Test]
+        public void Sum2And2()
+        {
+            Assert.AreEqual(2, 2);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void SumDecimal01And01And01()
+        {
+            decimal expectedResult = 0.3m;
+
+            decimal actualResult = Calculator.Calculator.SumDecimal3(0.1m, 0.1m, 0.1m);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+    }
+}
